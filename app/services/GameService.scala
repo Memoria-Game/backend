2cde3b5f932f07c2c28ce053518bb0e832138f31
francoisquellec <francois.quellec@heig-vd.ch>
2018-05-30 17:10:51 +0200

    game

--- app/services/GameService.scala
+++ app/services/GameService.scala
@@ -5,9 +5,9 @@ import utils.LevelGenerator
 
 class GameService {
   def createNewStage(u:User) = {
+
     val generator = new LevelGenerator(30, 30, 2, 2, 500)
-    generator.generateLevel() foreach { row => row foreach print; println }
-    println()
+
   }
 
   def winGame(u:User) = {
