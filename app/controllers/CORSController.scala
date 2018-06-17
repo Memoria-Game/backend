21766c7763c5e40caf63260f26e9ac81b9669964
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2018-06-17 16:56:44 +0200

    cors origin things

---
+++ app/controllers/CORSController.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import javax.inject.Inject
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+
+class CORSController @Inject()(cc: ControllerComponents) extends AbstractController(cc){
+  def headers = List(
+    "Access-Control-Allow-Origin" -> "*",
+    "Access-Control-Allow-Methods" -> "GET, POST, OPTIONS, DELETE, PUT",
+    "Access-Control-Max-Age" -> "3600",
+    "Access-Control-Allow-Headers" -> "Origin, Content-Type, Accept, Authorization",
+    "Access-Control-Allow-Credentials" -> "true"
+  )
+
+  def rootOptions = options("/")
+  def options(url: String) = Action { request =>
+    NoContent.withHeaders(headers : _*)
+  }
+}
