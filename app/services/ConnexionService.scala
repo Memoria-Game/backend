6d5d355fb4a6226b3e74a47090cabf89ea85e74d
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2018-06-18 20:55:56 +0200

    Hashing function

--- app/services/ConnexionService.scala
+++ app/services/ConnexionService.scala
@@ -1,5 +1,7 @@
 package services
 
+import java.security.MessageDigest
+
 import dao.{FriendsDAO, UserDAO, UserStatisticDAO}
 import javax.inject.Inject
 import models.User
@@ -12,7 +14,7 @@ import scala.concurrent.{Await, Future}
 
 class ConnexionService @Inject()(ud: UserDAO, friendsDAO: FriendsDAO, statisticDAO: UserStatisticDAO) {
 
-  def signin(username: String, password: String): Future[Option[User]] = ud.getUser(username, password)
+  def signin(username: String, password: String): Future[Option[User]] = ud.getUser(username, hash(password))
 
   def getUser(implicit request: Request[_]): Future[Option[User]] = getUser(request.session.get("user_id").getOrElse("-1").toLong)
 
@@ -27,12 +29,13 @@ class ConnexionService @Inject()(ud: UserDAO, friendsDAO: FriendsDAO, statisticD
   }
 
   def signup(username: String, password: String, mail: String, country: String): Future[Option[User]] = {
+    val hp = hash(password)
     exists(username).flatMap(_ match {
       case true => Future {
         None
       }
       case false => {
-        val OptionalUser = ud.insertUser(username, password, mail, country)
+        val OptionalUser = ud.insertUser(username, hp, mail, country)
         val user = Await.result(OptionalUser, Duration.Inf)
         statisticDAO.createUserStat(user.id.get)
         OptionalUser.map(Some(_))
@@ -45,6 +48,13 @@ class ConnexionService @Inject()(ud: UserDAO, friendsDAO: FriendsDAO, statisticD
 
   def isAllowed(implicit request: Request[_]) = {
     request.session.get("user_id").nonEmpty
+  }
+
+
+  def hash(str:String) = {
+    MessageDigest.getInstance("SHA-256")
+      .digest(str.getBytes("UTF-8"))
+      .map("%02x".format(_)).mkString
   }
 
 }
